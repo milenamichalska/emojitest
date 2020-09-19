47b764f9fe3d97c935b15485ba633745f9d435e5
Milena Michalska <milena.mm@wp.pl>
2020-09-19 19:59:02 +0200

    added fixture to impotr emojis

---
+++ dataset/make_fixture.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+emojis_dump = []
+
+with open('emojis.csv', encoding="utf8") as csvfile:
+    emojireader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for i, row in enumerate(emojireader):
+        fixture_object = {
+            "model": "emojiapp.emoji",
+            "pk": i,
+            "fields": {
+                "emoji": row[0],
+                "emojiName": row[1]
+            }
+        }
+        emojis_dump.append(fixture_object)
+
+with open("emoji_fixture.json", "w") as write_file:
+    json.dump(emojis_dump, write_file)
