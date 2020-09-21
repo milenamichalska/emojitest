c97eefd4ab84aa2d9e5d3a7acee02e85b8b7e6e4
Milena Michalska <milena.mm@wp.pl>
2020-09-21 21:28:15 +0200

    styled selecting emojis

--- emojifrontend/src/app/app.component.ts
+++ emojifrontend/src/app/app.component.ts
@@ -11,12 +11,27 @@ export class AppComponent implements OnInit {
   constructor(private emojiApi: EmojiApi) {}
 
   public emojis = [];
+  public selectedEmojis = [];
   public searchInput = '';
 
   searchEmojis(query) {
     this.emojiApi.getEmojis(query).subscribe((emojis) => {
       this.emojis = emojis;
     });
+  }
+
+  isSelected(emoji) {
+    return this.selectedEmojis.findIndex(e => e.emojiName === emoji.emojiName) !== -1;
+  }
+
+  select(emoji) {
+    console.log(this.selectedEmojis);
+    const i = this.selectedEmojis.findIndex(e => e.emojiName === emoji.emojiName);
+    if (i === -1) {
+      this.selectedEmojis.push(emoji);
+    } else {
+      this.selectedEmojis.splice(i, 1);
+    }
   }
 
   ngOnInit() {
